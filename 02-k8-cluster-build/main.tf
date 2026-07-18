87b6dd096364667d3049a5f4a3ed2a4f4db88d65
void269 <void269.269@gmail.com>
2026-07-18 09:37:42 -0400

    building process

---
+++ 02-k8-cluster-build/main.tf
@@ -0,0 +1,84 @@
+//Building the VPC and subnets for the Kubernetes cluster
+resource "aws_vpc" "vpc01" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = "AWS VPC for ${var.env}"
+    ENV = var.env
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc01.id
+
+  tags = {
+    Name = "VPC Internet Gateway"
+    ENV = var.env
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id  = aws_vpc.vpc01.id
+  cidr_block = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.aws-az-list, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+    ENV = var.env
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id  = aws_vpc.vpc01.id
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+  availability_zone  = element(var.aws-az-list, count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+    ENV = var.env
+  }
+
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc01.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+    ENV = var.env
+  }
+}
+
+resource "aws_route_table_association" "aws-rt" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id  = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+//Deploying the Kubernetes cluster EC2 instances
+resource "aws_instance" "ec2_k8_manager" {
+  ami = var.ami_image_id
+  count = 1
+  availability_zone = element(var.aws_az_list, 0)
+  instance_type = var.ec2_instance_type
+  key_name = var.ssh_key
+  vpc_security_group_ids = [aws_security_group.net_traffic.id]
+  subnet_id = aws_subnet.private[0].id
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "k8_manager"
+    ENV = var.env
+  }
+
+  
+}
---
+++ 02-k8-cluster-build/variables.tf
@@ -0,0 +1,85 @@
+// VPC Vars
+variable "cidr_block" {
+  description = "VPC CIDR Block"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type = list(string)
+  default = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "aws_az_list" {
+  description = "List of Availability Zones"
+  type = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+//EC2 Vars
+variable "http_port" {
+  description = "SG Inbound port number"
+  type = number
+  default = 80
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type = string
+  default = "us-east-1"
+}
+
+variable "env" {
+  description = "Environment"
+  type = string
+  default = "lab"
+}
+
+variable "ami_image_id" {
+  description = "AMI ID for Ubuntu Linux EC2 Instance"
+  type = string
+  default = "ami-0b6d9d3d33ba97d99"
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance Type"
+  type = string
+  default = "t2.micro"
+}
+/*
+variable "access_key" {
+  description = "IAM access key"
+  type = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  description = "IAM secret key"
+  type = string
+  sensitive = true
+}
+*/
+variable "ssh_key" {
+  description = "SSH Key to access EC2 instances"
+  type = string
+  default = "Demo-key-01"
+}
+
+variable "ssh_key_path" {
+  description = "Path to SSH key"
+  type        = string
+  default     = "/home/ec2-user/.ssh/Demo-key-01.pem"
+}
+
+variable "username" {
+  description = "Username for SSH access to EC2 instances"
+  type        = string
+  default     = "ec2-user"
+}
