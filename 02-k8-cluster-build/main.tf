7146026e9b973a84b38e43b71429d2414ec89b0f
void269 <void269.269@gmail.com>
2026-07-18 09:39:15 -0400

    fixed var

--- 02-k8-cluster-build/main.tf
+++ 02-k8-cluster-build/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id  = aws_vpc.vpc01.id
   cidr_block = element(var.public_subnet_cidrs, count.index)
-  availability_zone = element(var.aws-az-list, count.index)
+  availability_zone = element(var.aws_az_list, count.index)
   map_public_ip_on_launch = true
 
   tags = {
@@ -34,7 +34,7 @@ resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
   vpc_id  = aws_vpc.vpc01.id
   cidr_block = element(var.private_subnet_cidrs, count.index)
-  availability_zone  = element(var.aws-az-list, count.index)
+  availability_zone  = element(var.aws_az_list, count.index)
   map_public_ip_on_launch = false
 
   tags = {
