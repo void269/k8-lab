8816a8f2cf80e1839353122c13847347e270d7e1
void269 <void269.269@gmail.com>
2026-07-25 09:21:17 -0400

    updated output

--- 02-k8-cluster-build/outputs.tf
+++ 02-k8-cluster-build/outputs.tf
@@ -1,14 +1,22 @@
-output "vpc_id" {
-  value = aws_vpc.vpc01.id
-  description = "VPC ID"
+output "manager_public_ip" {
+  description = "Public IP address of the Kubernetes control plane"
+  value       = aws_instance.k8_manager.public_ip
 }
 
-output "vpc_public_subnets" {
-  value = aws_subnet.public[*].id
-  description = "List of public subnet created"
+output "manager_ssh_command" {
+  description = "SSH command for connecting to the Kubernetes control plane"
+  value       = "ssh -i \"${var.ssh_key_path}\" ${var.username}@${aws_instance.k8_manager.public_ip}"
 }
 
-output "vpc_private_subnets" {
-  value = aws_subnet.private[*].id
-  description = "List of private subnet created"
+output "worker_public_ips" {
+  description = "Public IP addresses of the Kubernetes worker nodes"
+  value       = aws_instance.k8_worker[*].public_ip
+}
+
+output "worker_ssh_commands" {
+  description = "SSH commands for connecting to the Kubernetes worker nodes"
+  value = [
+    for worker in aws_instance.k8_worker :
+    "ssh -i \"${var.ssh_key_path}\" ${var.username}@${worker.public_ip}"
+  ]
 }
